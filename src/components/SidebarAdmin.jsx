9d875f3d58e4e1069e78c1be94e628d73dbcf772
peerapattap@gmail.com <peerapattap@gmail.com>
2025-12-04 11:05:18 +0700

    tap

--- src/components/SidebarAdmin.jsx
+++ src/components/SidebarAdmin.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router'
+import { SettingsIcon ,MenuIcon,StoreIcon, TableIcon, HouseIcon, KeyIcon, CreditCardIcon, Banknote, BanknoteIcon, Percent, PercentIcon, ChefHatIcon, LogOutIcon, CircleArrowLeft, CircleArrowLeftIcon} from 'lucide-react'
+
 
 function SidebarAdmin() {
     const active = (isActive) => {
@@ -12,11 +14,20 @@ function SidebarAdmin() {
                     <h1 className="text-xl font-bold text-gray-800">Admin Dashboard</h1>
                     <p className="text-sm text-gray-500">ระบบบริหารจัดการร้านอาหาร</p>
                 </div>
-                <nav className="grow p-4">
-                    <NavLink to="/admin" end className={({ isActive }) => active(isActive)} >Dashbord</NavLink>
-                    <NavLink to="/admin/addmenu" className={({ isActive }) => active(isActive)}>จัดการเมนู</NavLink>
-                    <NavLink to="/admin/table" className={({ isActive }) => active(isActive)}>จัดการโต๊ะ</NavLink>
-                    <NavLink to="/admin/login" className={({ isActive }) => active(isActive)}>login</NavLink>
+                <nav className="grow p-4 flex flex-col">
+                    <div className="flex flex-col">
+                        <NavLink to="/" className={({ isActive }) => active(isActive)}> <CircleArrowLeftIcon /> กลับหน้าหลัก</NavLink>
+                        <NavLink to="/admin" end className={({ isActive }) => active(isActive)} ><HouseIcon /> Dashbord</NavLink>
+                        <NavLink to="/admin/kitchen" className={({ isActive }) => active(isActive)}><ChefHatIcon /> จัดการครัว</NavLink>
+                        <NavLink to="/admin/table" className={({ isActive }) => active(isActive)}><TableIcon /> จัดการโต๊ะ</NavLink>
+                        <NavLink to="/admin/addmenu" className={({ isActive }) => active(isActive)}><MenuIcon /> จัดการเมนู</NavLink>
+                        <NavLink to="/admin/billing" className={({ isActive }) => active(isActive)}><BanknoteIcon /> จัดการการบิล</NavLink>
+                        <NavLink to="/admin/discount" className={({ isActive }) => active(isActive)}><PercentIcon /> จัดการส่วนลด</NavLink>
+                        <NavLink to="/admin/ตั้งค่าร้าน" className={({ isActive }) => active(isActive)}><StoreIcon /> ตั้งค่าร้าน</NavLink>
+                    </div>
+                    <div className="mt-auto pt-4 border-t">
+                        <NavLink to="/admin/logout" className={({ isActive }) => active(isActive)}> <LogOutIcon /> ออกจากระบบ</NavLink>
+                    </div>
                 </nav>
             </aside >
         </div >
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,44 +1,47 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import MenuPage from "../pages/MenuPage";
-import CartPage from "../pages/CartPage";
-import SummaryPage from "../pages/SummaryPage";
-import UserLayout from "../layouts/UserLayout";
-import AuthPage from "../pages/AuthPage";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import MenuPage from '../pages/MenuPage';
+import CartPage from '../pages/CartPage';
+import SummaryPage from '../pages/SummaryPage';
+import UserLayout from '../layouts/UserLayout';
+import AuthPage from '../pages/AuthPage';
 
-import AdminPage from "../pages/AdminPage";
-import AdminDashboard from "../pages/AdminDashbord";
-import AdminAddMenu from "../pages/AdminAddMenu";
-import TableAdmin from "../pages/TableAdmin";
-import LoginPage from "../pages/AdminLoginPage";
-import BillingPage from "../pages/BillingPage";
-import SuperAdminPage from "@/pages/superAdmin/SuperAdminPage";
+import AdminPage from '../pages/AdminPage';
+import AdminDashboard from '../pages/AdminDashbord';
+import AdminAddMenu from '../pages/AdminAddMenu';
+import TableAdmin from '../pages/TableAdmin';
+import AdminShopSettingPage from '../pages/AdminShopSettingPage';
+import LoginPage from '../pages/AdminLoginPage';
+import BillingPage from '../pages/BillingPage';
+import SuperAdminPage from '@/pages/superAdmin/SuperAdminPage';
 
 const router = createBrowserRouter([
-  { path: "/billing", element: <BillingPage /> },
+  { path: '/billing', element: <BillingPage /> },
   {
-    path: "/",
+    path: '/',
     element: <UserLayout />,
     children: [
-      { path: "/", element: <MenuPage /> },
-      { path: "/cart", element: <CartPage /> },
-      { path: "/summary", element: <SummaryPage /> },
+      { path: '/', element: <MenuPage /> },
+      { path: '/cart', element: <CartPage /> },
+      { path: '/summary', element: <SummaryPage /> },
     ],
   },
-  { path: "/auth", element: <AuthPage /> },
-  { path: "/superAdmin", element: <SuperAdminPage /> },
+  { path: '/auth', element: <AuthPage /> },
+  { path: '/superAdmin', element: <SuperAdminPage /> },
   {
-    path: "/admin",
+    path: '/admin',
     element: <AdminPage />,
     children: [
       { index: true, element: <AdminDashboard /> },
-      { path: "addmenu", element: <AdminAddMenu /> },
-      { path: "table", element: <TableAdmin /> },
-      { path: '*', element: <AdminDashboard /> }
+      { path: 'addmenu', element: <AdminAddMenu /> },
+      { path: 'table', element: <TableAdmin /> },
+      { path: 'ตั้งค่าร้าน', element: <AdminShopSettingPage /> },
+      { path: '*', element: <AdminDashboard /> },
+      
     ],
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
   },
 ]);
